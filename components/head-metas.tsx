6145a8b6b5c341b34958fe5c85ce29485a03e040
Hanif Mifta <hanif@jakpat.net>
2023-08-06 03:24:17 +0700

    chore: initial documentation

---
+++ components/head-metas.tsx
@@ -0,0 +1,70 @@
+const HeadMetas = () => {
+  return (
+    <>
+      <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <title>Documentation</title>
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/favicons/apple-touch-icon.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/favicons/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/favicons/favicon-16x16.png"
+      />
+      <link rel="manifest" href="/favicons/site.webmanifest"></link>
+      <meta
+        name="description"
+        content="Documentation for Boilerplate Templater, a VS Code extension for creating tech-agnostic dynamic and customizable boilerplate templates. Get started, create templates, and streamline your development process."
+      />
+      <meta
+        name="keywords"
+        content="Boilerplate Templater, VS Code extension, documentation, templates, boilerplate, tech-agnostic, customizable, development"
+      />
+      <meta name="author" content="Hanif Miftahuddin" />
+
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content="https://vsctemplater.hanifmifta.dev" />
+      <meta property="og:title" content="Boilerplate Templater Documentation" />
+      <meta
+        property="og:description"
+        content="Documentation for Boilerplate Templater, a VS Code extension for creating tech-agnostic dynamic and customizable boilerplate templates."
+      />
+      <meta
+        property="og:image"
+        content="https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExMmc0cGQxZnVjYjRwcnN0MmsyeW1rbTVkdml1Z2J2MDZrYzQ1eGp3aCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/KWeMphu95b9LOLdpbS/giphy.gif"
+      />
+
+      <meta property="twitter:card" content="summary_large_image" />
+      <meta
+        property="twitter:url"
+        content="https://vsctemplater.hanifmifta.dev"
+      />
+      <meta
+        property="twitter:title"
+        content="Boilerplate Templater Documentation"
+      />
+      <meta
+        property="twitter:description"
+        content="Documentation for Boilerplate Templater, a VS Code extension for creating tech-agnostic dynamic and customizable boilerplate templates."
+      />
+      <meta
+        property="twitter:image"
+        content="https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExMmc0cGQxZnVjYjRwcnN0MmsyeW1rbTVkdml1Z2J2MDZrYzQ1eGp3aCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/KWeMphu95b9LOLdpbS/giphy.gif"
+      />
+
+      <link rel="canonical" href="https://vsctemplater.hanifmifta.dev" />
+    </>
+  );
+};
+
+export default HeadMetas;
--- theme.config.tsx
+++ theme.config.tsx
@@ -1,18 +1,40 @@
-import React from 'react'
-import { DocsThemeConfig } from 'nextra-theme-docs'
+import React from "react";
+import { DocsThemeConfig } from "nextra-theme-docs";
+import Image from "next/image";
+import HeadMetas from "./components/head-metas";
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  head: <HeadMetas />,
+  useNextSeoProps: () => ({
+    titleTemplate: "%s | Boilerplate Templater Documentation",
+  }),
+  logo: (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "row",
+        gap: "0.5rem",
+        alignItems: "center",
+      }}
+    >
+      <Image src="/icon.png" alt="Logo" width={30} height={30} unoptimized />
+      <span
+        style={{
+          fontWeight: "500",
+        }}
+      >
+        Boilerplate Templater
+      </span>
+    </div>
+  ),
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: "https://github.com/Cybernetics354/vscode-boilerplates-templater-docs",
   },
-  chat: {
-    link: 'https://discord.com',
-  },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase:
+    "https://github.com/Cybernetics354/vscode-boilerplates-templater-docs",
   footer: {
-    text: 'Nextra Docs Template',
+    text: "️Created by Hanif Miftahuddin",
   },
-}
+};
 
-export default config
+export default config;
